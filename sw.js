8277c8ede89800279eaa28c141f88e6bbe352619
muthutrbtestbatch-onlineexam <muthutrbtestbatch@gmail.com>
2026-03-17 12:41:11 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,3 @@
-const CACHE_NAME = 'muthu-tet-v1';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
